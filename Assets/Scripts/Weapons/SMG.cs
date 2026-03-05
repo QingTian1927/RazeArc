e80e1bcfc579560a86c67a492a1cf83f3cbf434b
BV. Dũng <buidungnd.2005@gmail.com>
2026-03-05 14:34:16 +0700

    refactor: clean up SMG firing logic and improve spread application

--- Assets/Scripts/Weapons/SMG.cs
+++ Assets/Scripts/Weapons/SMG.cs
@@ -14,7 +14,7 @@ public class SMG : WeaponBase
         isAutomatic = true;
 
         damage = 12f;
-        fireRate = 0.08f; 
+        fireRate = 0.08f;
         range = 80f;
 
         magazineSize = 30;
@@ -28,10 +28,12 @@ public class SMG : WeaponBase
     protected override void Fire()
     {
         Camera camera = Camera.main;
-        Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        Ray centerRay = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        Vector3 spreadDirection = ApplySpread(centerRay.direction);
+        Ray ray = new Ray(camera.transform.position, spreadDirection);
 
         RaycastHit hit;
-
         Debug.DrawRay(ray.origin, ray.direction * range, Color.green, 0.3f);
 
         if (Physics.Raycast(ray, out hit, range, shootMask))
@@ -43,5 +45,14 @@ public class SMG : WeaponBase
 
             Debug.Log("Hit: " + hit.collider.name);
         }
+    }
+
+    Vector3 ApplySpread(Vector3 baseDirection)
+    {
+        float spread = 1.5f;
+        float yaw = Random.Range(-spread, spread);
+        float pitch = Random.Range(-spread, spread);
+
+        return Quaternion.Euler(pitch, yaw, 0f) * baseDirection;
     }
 }
